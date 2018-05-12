1e63416103c8f3e6e53ddb5b909d6ad69733fc97
unknown <bank_28185@hotmail.com>
2018-05-12 21:46:13 +0700

    Add random GPS location function

---
+++ random.js
@@ -0,0 +1,8 @@
+const random = (x, y) => {
+  return {
+    x: Number(`${x}.${Math.floor(Math.random() * 1000000 + 1)}`),
+    y: Number(`${y}.${Math.floor(Math.random() * 1000000 + 1)}`)
+  };
+};
+
+console.log(random(14, 100));
